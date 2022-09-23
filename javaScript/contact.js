2abaad69ef05fd0df19635c75c0bb90636532dde
Rahul Sharma <rahul.sharma@Technovert.com>
2022-09-23 14:42:54 +0530

    Website

--- javaScript/contact.js
+++ javaScript/contact.js
@@ -2,9 +2,10 @@ var button =  document.getElementsByClassName('second-button');
 for( var  i = 0 ; i< button.length ; i++){
   button[i].addEventListener('click' , function() {
           document.getElementById("myform").reset();
-          document.getElementsByClassName("formerror")[i].innerHTML=" ";
+          location.reload(); 
   })
 }
+
 var gender =  document.getElementsByClassName('male');
 for( var  i = 0 ; i< gender.length ; i++){
   gender[i].addEventListener('click' , function() {
@@ -20,25 +21,37 @@ for( var  i = 0 ; i< gender.length ; i++){
 
 function validateForm(){
   var res = true;
-  var k = document.getElementById("name").value;
-  if(k.length==0){
+  var name = document.getElementById("name").value;
+  if(name.length==0){
     document.getElementsByClassName("formerror")[0].innerHTML=" *Name is required";
     res=false;
   }
-  var k = document.getElementById("email").value;
-  if(k.length==0){
+  if(name.length>0){
+    document.getElementsByClassName("formerror")[0].innerHTML=" *";
+    res=true;
+  }
+
+  var email = document.getElementById("email").value;
+  if(email.length==0){
     document.getElementsByClassName("formerror")[1].innerHTML=" *Email is required";
     res=false;
   }
-  var k = document.getElementById("orgName").value;
-  if(k.length==0){
+  if(email.length>0){
+    document.getElementsByClassName("formerror")[1].innerHTML=" *";
+    res=true;
+  }
+  var org = document.getElementById("orgName").value;
+  if(org.length>0){
+    document.getElementsByClassName("formerror")[3].innerHTML=" *";
+    res=true;
+  }
+  if(org.length==0){
     document.getElementsByClassName("formerror")[3].innerHTML=" *Organization required";
     res=false;
   }
   if (res==false){
     document.getElementsByClassName("error-show")[0].innerHTML="Please fill all the required fields below";
   }
-
   return res;
   
   }
@@ -49,4 +62,14 @@ function setPromotion(value) {
   else {
       document.getElementById("promotion").value = "";
   }
+}
+
+var button =  document.getElementsByClassName('second-button');
+for( var  i = 0 ; i< button.length ; i++){
+  button[i].addEventListener('click' , function() {
+          var error = document.getElementsByClassName("formerror");
+          for(var i=0 ; i<error.length ; i++){
+            error[i].innerHTML=" ";
+          }
+  })
 }
