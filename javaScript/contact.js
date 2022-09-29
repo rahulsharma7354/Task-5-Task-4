5b1a6cd94603cb5ceb980d3ba7ef65a62aadb947
Rahul Sharma <rahul.sharma@Technovert.com>
2022-09-29 19:28:02 +0530

    website

--- javaScript/contact.js
+++ javaScript/contact.js
@@ -1,8 +1,83 @@
+$(document).ready(function () {
+  $(".male").click(function () {
+    var k = $('input[name="male"]:checked').val();
+    alert(k);
+  })
+  $('.second-button').click(function () {
+    $('#myform')[0].reset();
+    localStorage.clear();
+  })
+}
+)
+$('.first-button').click(function validateForm() {
+  var res = true;
+  var name = $('#name').val();
+  if (name.length == 0) {
+    $(".input-text-name").addClass("color");
+    $(".star")[0].style.visibility = "hidden";
+    $(".formerror")[0].style.visibility = "visible";
+    res = false;
+  }
+  var email = $("#email").val();
+  if (email.length == 0) {
+    $(".input-text-email").addClass("color");
+    $(".star")[1].style.visibility = "hidden";
+    $(".formerror")[1].style.visibility = "visible";
+    res = false;
+  }
+  var org = $("#orgName").val();
+  if (org.length == 0) {
+    $(".orgname").addClass("color");
+    $(".star")[2].style.visibility = "hidden";
+    $(".formerror")[3].style.visibility = "visible";
+    res = false;
+  }
+  if (res==false){
+    $(".error-show")[0].style.visibility = "visible";
+  }
+  if (res==true) {
+    var uname = $("#name"). val();
+    var uemail = $("#email").val();
+    var uphone = $("#phone").val();
+    var uorgname = $("#orgName").val();
+     var ucity = $("#city").val();
+    var ustate = $("#stateDropdown").val();
+    var ucontact = $(".contact-details").val();
+    var ugender = $(".male").val();
+    var promo =$(".promotion").val();
+    var website = $(".input-text-website").val();
+    
+    
+    localStorage.setItem('Name' , uname);
+    localStorage.setItem('Email' , uemail);
+    localStorage.setItem('Phone' , uphone);
+    localStorage.setItem('Organization' , uorgname);
+    localStorage.setItem('City', ucity);
+    localStorage.setItem('State' , ustate);
+    localStorage.setItem('Contact' , ucontact);
+    localStorage.setItem('Gender' , ugender);
+    localStorage.setItem('Promotion Code' , promo);
+    localStorage.setItem('Website',website);
+  } else {
+    return res;
+  }
+})
+$('.stateDropdown').change( function () {
+  var k = $('.stateDropdown').val();
+  $('.promotion').val(k + " - PROMO");
+  
+})
+
+
+
+
+
+//----------------------------JAVASCRIPT--------------------------------------------//
 // var button =  document.getElementsByClassName('second-button');
 // for( var  i = 0 ; i< button.length ; i++){
 //   button[i].addEventListener('click' , function() {
 //           document.getElementById("myform").reset();
-//           location.reload(); 
+//           location.reload();
 //   })
 // }
 // var gender = document.getElementsByClassName("male");
@@ -12,55 +87,6 @@
 //     alert(k)
 //   })
 // }
-$(document).ready(function(){
-  $( ".second-button" ).click(function() {
-    $("#myform").reset();
-    location.reload(true);
-  });
-
-  $(".male").click(function(){
-      var k = $('input[name="male"]:checked').val();
-      alert(k);
-  })
-  }
-)
-
- function validateForm() {
-  var res = true;
-var name = $("#name").val();
-if(name.length==0){
-$(".input-text-name").css({"border-color":"red"});
-$(".star")[0].css('visibility', 'hidden');
-$(".formerror")[0].css('visibility', 'visible');
-res=false;
-}
-var email = $("#email").val();
-if(email.length==0){
-$(".input-text-email").css({"border-color":"red"});
-$(".star")[1].css('visibility', 'hidden');
-$(".formerror")[1].css('visibility', 'visible');
-res=false;
-}
-var org = $("#orgName").val();
-if(org.length==0){
-$(".input-text-phone")[1].css({"border-color":"red"});
-$(".star")[2].css('visibility', 'hidden');
-$(".formerror")[3].css('visibility', 'visible');
-res=false;
-}
-if (res==false){
-  $("error-show")[0].css('visibility', 'visible');
-}
-if (res==true) {
-  alert("Form Submitted");
-} else {
- return res;
-}
-}
-    
-
-
-
 //  function validateForm(){
 //   var res = true;
 //   var name = document.getElementById("name").value;
@@ -93,8 +119,6 @@ if (res==true) {
 //     return res;
 //   }
 //   }
-
-
 // function setPromotion(value) {
 //   if (value != 'select') {
 //       document.getElementById("promotion").value = value + " - PROMO";
